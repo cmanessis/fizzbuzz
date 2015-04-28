a444a58674ed0d79e948902db3198b4d2de72d0f
CHRIS MANESSIS <cmpetrowii@gmail.com>
2015-04-28 12:08:58 +0100

    fizzbuzz oneline

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,10 @@
 def fizzbuzz(number)
-   return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
-   return 'fizz' if number % 3 == 0
-   return 'buzz' if number % 5 == 0
-   return number
+   number % 3 == 0 || number % 5 == 0 ? "#{'fizz' if number % 3 == 0}" + "#{'buzz' if number % 5 == 0}" : number
 end
+
+=begin
+puts fizzbuzz(1) # == 1
+puts fizzbuzz(3) # == 3
+puts fizzbuzz(5) # == 5
+puts fizzbuzz(15) # == 5
+=end
