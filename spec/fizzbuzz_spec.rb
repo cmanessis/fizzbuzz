af708dcc946a797a6961a25d3cac27594d61cc3e
CHRIS MANESSIS <cmpetrowii@gmail.com>
2015-04-28 17:11:52 +0100

    extend-method success

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,23 +4,23 @@ require "fizzbuzz"
 describe 'fizzbuzz' do
 
   it 'Expect 1 to equal 1' do
-    expect(fizzbuzz(1)).to eq 1
+    expect(1.fizzbuzz).to eq 1
   end
 
   it 'Expect 3 to equal "fizz"' do
-    expect(fizzbuzz(3)).to eq "fizz"
+    expect(3.fizzbuzz).to eq "fizz"
   end
 
   it 'Expect 5 to equal "buzz"' do
-    expect(fizzbuzz(5)).to eq "buzz"
+    expect(5.fizzbuzz).to eq "buzz"
   end
 
   it 'Expect 15 to equal "fizzbuzz"' do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(15.fizzbuzz).to eq "fizzbuzz"
   end
 
   it 'Expect 101 to equal 101' do
-    expect(fizzbuzz(101)).to eq 101
+    expect(101.fizzbuzz).to eq 101
   end
 
 
