d7d4f3214faa506264bfe248bf117b85bfa4cd5d
CHRIS MANESSIS <cmpetrowii@gmail.com>
2015-04-28 15:30:03 +0100

    updated rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
    number % 3 == 0 || number % 5 == 0 ? "#{'fizz' if number % 3 == 0}" + "#{'buzz' if number % 5 == 0}" : number
-end
+end 
 
 =begin
 puts fizzbuzz(1) # == 1
 puts fizzbuzz(3) # == 3
 puts fizzbuzz(5) # == 5
-puts fizzbuzz(15) # == 5
+puts fizzbuzz(15) # ==15
 =end
